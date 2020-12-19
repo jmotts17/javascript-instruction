b86e0484a65555122754c540a10b4f155e2efb06
Josh Motta <joshua.a.motta@gmail.com>
2020-12-18 22:13:21 -0500

    init commit

---
+++ ch4_FindMax.js
@@ -0,0 +1,28 @@
+function findMax(numArr) {
+    let maxNum = 0;
+
+    for(let i = 0; i < numArr.length; i++) {
+        if(numArr[i] > maxNum) {
+            maxNum = numArr[i];
+        }
+    }
+    return maxNum;
+}
+
+function findMaxAndIndex(numArr) {
+    let maxNum = 0;
+    let maxIndex = 0;
+
+    for(let i = 0; i < numArr.length; i++) {
+        if(numArr[i] > maxNum) {
+            maxNum = numArr[i];
+            maxIndex = i;
+        }
+    }
+
+    return { max: maxNum, index: maxIndex};
+}
+
+console.log(findMax([12, 35, 6]));
+
+console.log(findMaxAndIndex([12, 35, 6]));
