b2a74b40d0434e540b4786dca48e057059a5d9d1
Josh Motta <joshua.a.motta@gmail.com>
2020-12-19 09:04:52 -0500

    init commit

---
+++ Saturday/mAndC.js
@@ -0,0 +1,14 @@
+function timeForMilkAndCookies(date) {
+    
+    // if(date.getMonth() == 11 && date.getDate() == 24) {
+    //     return true;
+    // }
+
+    // return false;
+
+    return date.getMonth() == 11 && date.getDate() == 24;
+}
+
+console.log(timeForMilkAndCookies(new Date(2013, 11, 24)));
+console.log(timeForMilkAndCookies(new Date(2013, 0, 23)));
+console.log(timeForMilkAndCookies(new Date(3000, 11, 24)));
